908724f3cd7e0e4d4a1b74d40f660c1e7a61082f
Pro100-Adm <kirow.alesha@yandex.ru>
2022-11-12 11:11:26 +0300

    Hi (new)

---
+++ Example005/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if(username == "Алексей")
+{
+Console.WriteLine("Ура, это же АЛЕКСЕЙ!");
+}
+else
+{
+Console.Write("Привет, ");
+Console.WriteLine(username);
+}
