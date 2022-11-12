61135bbccf19aad77b19792ed252c00cb0bd0bec
Pro100-Adm <kirow.alesha@yandex.ru>
2022-11-12 11:02:02 +0300

    Div

---
+++ Example004/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+float AskUser()
+{
+Console.Write("Введите число: ");
+string Ask = Console.ReadLine();
+try
+{
+    float op = float.Parse(Ask);
+    return op;
+}
+catch (Exception)
+{
+    Console.WriteLine("Введите верное число!");
+    return AskUser();
+}
+}
+
+float Div(float op1, float op2)
+{
+    float result;
+    result = op1/op2;
+    Console.WriteLine(result);
+    return result;
+}
+
+
+Div(AskUser(), AskUser());
